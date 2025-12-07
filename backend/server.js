7b05134033e2b65a3bfa56bdaa53f05c079b9ac4
aivinalex <aivinalexphilip@gmail.com>
2025-12-07 11:01:19 +0530

    learned fastifyStatic

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-"strict";
+"use strict";
+
+import fastify from "fastify";
+import path from "path";
+import fastifyStatic from "@fastify/static";
+
+const app = fastify({ logger: true });
+app.register(fastifyStatic, {
+  root: path.join(process.cwd(), "public"),
+  prefix: "/",
+});
+app.get("/", async function routesHandler(request, reply) {
+  try {
+    return reply.sendFile("caselist.html");
+  } catch (err) {
+    response.code(500).send(err.message);
+  }
+});
+
+try {
+  await app.listen({ port: 3000 });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
